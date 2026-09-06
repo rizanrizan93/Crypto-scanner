7bb96ebd60292a519f0b88a5fb6e742a8ef3040f
rizanrizan93 <rizanrizan93@gmail.com>
2026-09-06 15:06:50 +0700

    style: remove unused linkage import

--- src/crypto_scanner/linkage_cycle.py
+++ src/crypto_scanner/linkage_cycle.py
@@ -2,7 +2,6 @@ from __future__ import annotations
 
 import json
 import time
-from decimal import Decimal
 
 from crypto_scanner.binance.microstructure import (
     BinanceDemoMicrostructureClient,
